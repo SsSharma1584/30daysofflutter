68a168272976a694c5fc705e21ba5c250d9bbfdf
SsSharma1584 <ss1109254@gmail.com>
2023-03-14 20:29:02 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/home_page.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Center(child: Text('Welcome to 30 days of flutter')),
-        ),
-      ),
-    );
 
+    int days=30;
+
+    String name= "SsSharma";
+
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
